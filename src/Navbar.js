3ea95d821ab71ee67fe32e52e6a9dfd96d8b5a7f
sw-1113 <sunny9040@naver.com>
2025-12-20 13:40:50 +0900

    oss-final

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,31 +1,59 @@
-import './nav.css'
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import './nav.css';
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import icon from "./image/icon.png";
 
 function Navbar() {
-
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const user = localStorage.getItem("user");
+        setIsLoggedIn(!!user);
+    }, [location.pathname]);
+
+    const handleLogout = () => {
+        localStorage.removeItem("user");
+        setIsLoggedIn(false);
+        navigate("/Login");
+    };
 
     return (
         <nav>
             <ul>
-                <li className="nav-left">
-                    <Link to="Main"><h2>My Fitness Tracker</h2></Link>
-                </li>
-
-                <li className="right">
+                <div className="left">
                     <li>
-                        <Link to="Search">Search</Link>
+                        <Link to="/Main">
+                            <h2>My Fitness Tracker</h2>
+                        </Link>
                     </li>
-                    {isLoggedIn ? (
-                        <img src={icon}/>
-                    ) : (
-                        <>
-                            <Link to="Login" className="nav-link">Login/sign up</Link>
-                        </>
-                    )}
-                </li>
+                </div>
+
+                <div className="right">
+                    <ul>
+                        <li><Link to="/Record">Record</Link></li>
+                        <li><Link to="/Mypage">Mypage</Link></li>
+                        <li><Link to="/Search">Search</Link></li>
+
+                        {isLoggedIn ? (
+                            <li>
+                                <Link to="/Mypage">
+                                    <img
+                                        src={icon}
+                                        alt="profile"
+                                        className="profile-icon"
+                                    />
+                                </Link>
+                                <button type="button" onClick={handleLogout}>Logout</button>
+                            </li>
+                        ) : (
+                            <li>
+                                <Link to="/Login">Sign In</Link>
+                            </li>
+                        )}
+                    </ul>
+                </div>
             </ul>
         </nav>
     );
